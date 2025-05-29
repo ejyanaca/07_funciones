bf04e394a144174b3ef9b0fd73411627e8651d2d
ejyanaca <elvisyanacayo@gmail.com>
2025-05-29 14:53:12 -0500

    ejercicio 2: Calculo del resto y cociente a traves de restas

---
+++ funcion2.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int residuo(int,int);
+int cociente(int,int);
+int main(){
+	int num1,num2,res,coc;
+	cout<<"Ingrese el primer numero: ";
+	cin>>num1;
+	cout<<"Ingrese el segundo numero: ";
+	cin>>num2;
+	res=residuo(num1,num2);
+	cout<<"El residuo de la division es: "<<res<<endl;
+	coc=cociente(num1,num2);
+	cout<<"El cociente de la divison es: "<<coc<<endl;
+	  return 0;
+}
+int residuo(int a,int b){
+	while(a>=b){
+		a=a-b;
+	}
+	return a;		
+}
+int cociente(int c,int d){
+	int coci=0;
+	while(c>=d){
+		c=c-d;
+		coci=coci+1;
+	}
+	return coci;	
+}
+
