43c5255eac9a09a0918f0d11f99e73deb75d8770
ejyanaca <elvisyanacayo@gmail.com>
2025-05-30 10:26:49 -0500

    ejercicio3:Identificar si dos numeros son amigos

---
+++ ejercicio3.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+    int divisores(int);
+    int divisoress(int); 
+    int main(){
+    	int n1,n2,s;
+    	cout<<"Ingrese el primer numero: "<<endl;
+    	cin>>n1;
+    	cout<<"Ingrese el segundo numero: "<<endl;
+    	cin>>n2;
+    	   if(divisores(n1)&&divisoress(n2)){
+    	   	cout<<"Los numeros son amigos.";
+		   } else {
+		    cout<<"Los numeros no son amigos.";
+		    
+		   }
+    	  return 0; 
+	}
+	int divisores(int m){
+		int s=0;
+		for(int i=1;i<m;i++){
+			if(m%i==0){
+				s=s+i;
+			}
+		}
+		return s;
+	}
+	int divisoress(int n){
+		int p=0;
+		for(int i=1;i<n;i++){
+			if(n%i==0){
+				 p=p+i;
+			}
+		}
+		 return p;
+	}
+	
