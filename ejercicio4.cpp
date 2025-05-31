ddc84fb9d35c10417956ee89ba274fb7cf03732d
ejyanaca <elvisyanacayo@gmail.com>
2025-05-31 17:43:09 -0500

    ejercicio4: Calcular la combinatoria de dos numeros.

---
+++ ejercicio4.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int factorial(int);
+int main(){
+	int m,n,c;
+	cout<<"Combinatoria de dos numeros."<<endl;
+	cout<<"Ingrese el numero de elementos: ";
+	cin>>m;
+	cout<<"Ingrese el numero de elementos seleccionados o agrupados: ";
+	cin>>n;
+	c=factorial(m)/(factorial(n)*factorial(m-n));
+	cout<<"La combinatoria de "<<m<<" y "<<n<<" es: "<<c<<endl;
+	return 0;
+}
+int factorial (int n){
+	int f=1;
+	for(int i=2; i<=n; i++){
+			f=f*i;
+	}
+	return f;
+}
